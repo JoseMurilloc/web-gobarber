3fe4395cb2a46cb56900a7b1aee58017a7951d98
Jose Murillo <jooseemurillo@gmail.com>
2020-10-15 01:11:52 -0300

    Messages erros of input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { Background, Container, Content } from './styles';
 import logo from '../../assets/logo.svg';
@@ -9,22 +9,32 @@ import Button from '../../components/Button';
 import { Form } from '@unform/web';
 
 import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignUp: React.FC = () => {
   
+  const formRef = useRef<FormHandles>(null);
+
+  
   const handleSubmit = useCallback(async(data: object) => {
    try {
-     const scheme = Yup.object().shape({
-       name: Yup.string().required('Nome obrigatório'),
-       email: Yup.string().required('E-mail obrgatório').email('Digite um email válido'),
-       password: Yup.string().min(6, 'No minimo 6 dígitos'),
-     });
+    formRef.current?.setErrors({});   
+    const scheme = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      email: Yup.string().required('E-mail obrgatório').email('Digite um email válido'),
+      password: Yup.string().min(6, 'No minimo 6 dígitos'),
+    });
 
-     await scheme.validate(data, {
-       abortEarly: false
-     })
+    await scheme.validate(data, {
+      abortEarly: false
+    })
+
    } catch(err) {
-      console.log(err);
+    
+    const errors = getValidationErrors(err)
+
+    formRef.current?.setErrors(errors)   
    }
   }, [])
 
@@ -34,7 +44,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logo} alt="Gobarber"/>
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input 
